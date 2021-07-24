ce74a11f774b2ca4bad99e8ef1ecc43adbab1888
emdadul_islam <emdadulislam439@gmail.com>
2021-07-24 09:26:26 +0600

    added viewBinding

--- app/src/main/java/com/example/lesson_2_part_1_about_me/MainActivity.kt
+++ app/src/main/java/com/example/lesson_2_part_1_about_me/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.lesson_2_part_1_about_me
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lesson_2_part_1_about_me.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
