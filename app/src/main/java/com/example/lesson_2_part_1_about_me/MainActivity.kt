7c321c20db4e80db107d8c81da643dbc8a2d6e87
emdadul_islam <emdadulislam439@gmail.com>
2021-07-24 10:37:05 +0600

    added clickListener for "done button" and set visibility for different views

--- app/src/main/java/com/example/lesson_2_part_1_about_me/MainActivity.kt
+++ app/src/main/java/com/example/lesson_2_part_1_about_me/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.lesson_2_part_1_about_me
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.lesson_2_part_1_about_me.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnDone.setOnClickListener {
+            addNickName(it)
+        }
+    }
+    private fun addNickName(view : View){
+        binding.tvNickName.text = binding.etNickName.text
+        binding.etNickName.visibility = View.GONE
+        view.visibility = View.GONE
+        binding.tvNickName.visibility = View.VISIBLE
+
+        //hide the keyboard
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE)  as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
