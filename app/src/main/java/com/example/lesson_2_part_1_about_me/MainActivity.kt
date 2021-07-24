2ef5a90c84f0f87c41e646f235feb221bd72cd62
emdadul_islam <emdadulislam439@gmail.com>
2021-07-24 09:05:10 +0600

    initial commit

---
+++ app/src/main/java/com/example/lesson_2_part_1_about_me/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lesson_2_part_1_about_me
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
