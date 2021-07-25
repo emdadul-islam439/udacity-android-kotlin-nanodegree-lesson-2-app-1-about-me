aaf9450a3accbb86a7a66c4bcba807da20b93a74
emdadul_islam <emdadulislam439@gmail.com>
2021-07-25 14:54:41 +0600

    added "Data Binding"...

--- app/src/main/java/com/example/lesson_2_part_1_about_me/MainActivity.kt
+++ app/src/main/java/com/example/lesson_2_part_1_about_me/MainActivity.kt
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
 import com.example.lesson_2_part_1_about_me.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(binding.root)
 
         binding.btnDone.setOnClickListener {
@@ -18,11 +19,13 @@ class MainActivity : AppCompatActivity() {
         }
     }
     private fun addNickName(view : View){
-        binding.tvNickName.text = binding.etNickName.text
-        binding.etNickName.visibility = View.GONE
-        view.visibility = View.GONE
-        binding.tvNickName.visibility = View.VISIBLE
-
+        binding.apply {
+            tvNickName.text = binding.etNickName.text
+            invalidateAll()
+            etNickName.visibility = View.GONE
+            view.visibility = View.GONE
+            tvNickName.visibility = View.VISIBLE
+        }
         //hide the keyboard
         val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE)  as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
