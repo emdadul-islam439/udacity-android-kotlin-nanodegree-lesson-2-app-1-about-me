87d05b985182065f882ce7e568b623ed372dd883
emdadul_islam <emdadulislam439@gmail.com>
2021-07-25 15:28:19 +0600

    added "<data>" section in activity_main.xml ||  if don't update the "appText" data class then "NickName" won't be updated.

--- app/src/main/java/com/example/lesson_2_part_1_about_me/MainActivity.kt
+++ app/src/main/java/com/example/lesson_2_part_1_about_me/MainActivity.kt
@@ -6,19 +6,31 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import com.example.lesson_2_part_1_about_me.databinding.ActivityMainBinding
+import com.example.lesson_2_part_1_about_me.model.AppText
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
+    lateinit var appText: AppText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(binding.root)
 
+        appText = AppText(
+            getString(R.string.emdadul_islam),
+            "",
+            getString(R.string.sample_bio_text),
+            getString(R.string.nick_name_edit_text_hint),
+            getString(R.string.done)
+        )
+        binding.appText = appText
+
         binding.btnDone.setOnClickListener {
             addNickName(it)
         }
     }
-    private fun addNickName(view : View){
+
+    private fun addNickName(view: View) {
         binding.apply {
             tvNickName.text = binding.etNickName.text
             invalidateAll()
@@ -27,7 +39,7 @@ class MainActivity : AppCompatActivity() {
             tvNickName.visibility = View.VISIBLE
         }
         //hide the keyboard
-        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE)  as InputMethodManager
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
---
+++ app/src/main/java/com/example/lesson_2_part_1_about_me/model/AppText.kt
@@ -0,0 +1,9 @@
+package com.example.lesson_2_part_1_about_me.model
+
+data class AppText(
+    var mainName : String = "",
+    var nickName : String = "",
+    var bioText : String = "",
+    var editTextHint : String = "",
+    var doneButtonText : String = "",
+)
